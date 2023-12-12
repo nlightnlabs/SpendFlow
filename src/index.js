ae5c2db0595f412976ededfaf27fbf24a09891ce
nlightnlabs <@nlightnlabs.com>
2023-12-11 17:41:54 -0800

    third commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import ReactDOM from 'react-dom/client';
+import {ContextProvider, Context } from './components/Context';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>
 );
 
