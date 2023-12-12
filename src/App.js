79261f1b3daaaec4248377a2dd17c7c8f5c8e8ec
nlightnlabs <@nlightnlabs.com>
2023-12-11 17:07:03 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
+function App() {
+
+
+  return (
+    <div className="App">
+      {window.location.assign("https://freeagent.network")}
+    </div>
+  );
+}
+
+export default App;
